14f5cbe11c4d787015d773526611f64049dda6de
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2025-06-10 15:23:42 +0530

    Code initial

--- src/components/Auth/Signup.tsx
+++ src/components/Auth/Signup.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { TextField, Button, Typography, Box, IconButton, InputAdornment } from "@mui/material";
+import { TextField, Button, Typography, Box, IconButton, InputAdornment, Checkbox, Divider } from "@mui/material";
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
@@ -25,93 +25,108 @@ const Signup = () => {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.2 }}
-          className="w-full sm:max-w-sm md:max-w-md bg-zinc-800 p-6 md:p-10 rounded-2xl shadow-2xl"
         >
-          <Typography
+          <Divider>Or</Divider>
+          <div className="w-full sm:max-w-sm md:max-w-md bg-zinc-800 p-6 md:p-10 rounded-2xl shadow-2xl">
+
+            {/* <Typography
             variant="h5"
             className="text-center font-bold pb-6 text-white"
-          >
+            >
             Create Your Account
-          </Typography>
-          <form className="flex flex-col gap-6">
-            <TextField
-              fullWidth
-              label="Enter your full name"
-              className="bg-zinc-950 rounded-lg" 
-              variant="outlined"
-              InputProps={{ style:{ color: '#fff' } }}
-              InputLabelProps={{ style: { color: '#94a3b8' } }}
-            />
-            <TextField
-              fullWidth
-              label="Your email address"
-              type="email"
-              variant="outlined"
-              InputProps={{ style: { color: '#fff' } }}
-              className="bg-zinc-950 rounded-lg"
-              InputLabelProps={{ style: { color: '#94a3b8' } }}
-            />
-            <TextField
-              fullWidth
-              label="Your password"
-              type={showPassword ? "text" : "password"}
-              variant="outlined"
-              className="bg-zinc-950 rounded-lg"
-              InputProps={{
-                style: { color: '#fff' },
-      
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton onClick={togglePasswordVisibility} edge="end">
-                      {showPassword ? (
-                        <VisibilityOff style={{ color: '#94a3b8' }} />
-                      ) : (
-                        <Visibility style={{ color: '#94a3b8' }} />
-                      )}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              InputLabelProps={{ style: { color: '#94a3b8' } }}
-            />
-            <TextField
-              fullWidth
-              label="Confirm your password"
-              type={showConfirmPassword ? "text" : "password"}
-              variant="outlined"
-              className="bg-zinc-950 rounded-lg"
-              InputProps={{
-                style: { color: '#fff' },
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton onClick={toggleConfirmPasswordVisibility} edge="end">
-                      {showConfirmPassword ? (
-                        <VisibilityOff style={{ color: '#94a3b8' }} />
-                      ) : (
-                        <Visibility style={{ color: '#94a3b8' }} />
-                      )}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              InputLabelProps={{ style: { color: '#94a3b8' } }}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              fullWidth
-              sx={{ mt: 1, py: 1.5, fontWeight: 'bold', textTransform: 'none', borderRadius: '12px' }}
-            >
-              Sign Up
-            </Button>
-            <Typography
-              onClick={() => navigate("/login")}
-              className="text-sm text-sky-400 text-center mt-4 cursor-pointer hover:underline"
-            >
-              Already have an account?
-            </Typography>
-          </form>
+            </Typography> */}
+            <form className="flex flex-col ">
+              <p className=" mb-1">Full Name</p>
+              <TextField
+                fullWidth
+                label="Enter your full name"
+                className="bg-zinc-950 rounded-lg "
+                variant="outlined"
+                InputProps={{ style: { color: '#fff' } }}
+                InputLabelProps={{ style: { color: '#94a3b8' } }}
+              />
+              <p className="mt-4 mb-1">Email</p>
+              <TextField
+                fullWidth
+                label="Your email address"
+                type="email"
+                variant="outlined"
+                InputProps={{ style: { color: '#fff' } }}
+                className="bg-zinc-950 rounded-lg "
+                InputLabelProps={{ style: { color: '#94a3b8' } }}
+              />
+              <p className="mt-4 mb-1">Password</p>
+              <TextField
+                fullWidth
+                label="Your password"
+                type={showPassword ? "text" : "password"}
+                variant="outlined"
+                className="bg-zinc-950 rounded-lg"
+                InputProps={{
+                  style: { color: '#fff' },
+
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={togglePasswordVisibility} edge="end">
+                        {showPassword ? (
+                          <VisibilityOff style={{ color: '#94a3b8' }} />
+                        ) : (
+                          <Visibility style={{ color: '#94a3b8' }} />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                InputLabelProps={{ style: { color: '#94a3b8' } }}
+              />
+              <p className="mt-4 mb-1">Confirm Password</p>
+              <TextField
+                fullWidth
+                label="Confirm your password"
+                type={showConfirmPassword ? "text" : "password"}
+                variant="outlined"
+                className="bg-zinc-950 rounded-lg"
+                InputProps={{
+                  style: { color: '#fff' },
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={toggleConfirmPasswordVisibility} edge="end">
+                        {showConfirmPassword ? (
+                          <VisibilityOff style={{ color: '#94a3b8' }} />
+                        ) : (
+                          <Visibility style={{ color: '#94a3b8' }} />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                InputLabelProps={{ style: { color: '#94a3b8' } }}
+              />
+              <Typography
+                className="text-sm text-center mt-4 pt-5"
+              >
+                <Checkbox
+                  sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }} />
+                I agree to the <span onClick={() => navigate("/terms")} className="text-sky-400 cursor-pointer hover:underline">Terms of Service</span> and <span onClick={() => navigate("/privacy")} className="text-sky-400 cursor-pointer hover:underline">Privacy Policy</span>
+              </Typography>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+                sx={{ mt: 3, py: 1.5, fontWeight: 'bold', textTransform: 'none', borderRadius: '12px' }}
+
+              >
+                Create Account
+              </Button>
+              <Typography
+                onClick={() => navigate("/login")}
+                className="text-sm text-sky-400 text-center mt-4 cursor-pointer hover:underline pt-5"
+              >
+                Already have an account?
+              </Typography>
+            </form>
+          </div>
         </motion.div>
       </Box>
 
